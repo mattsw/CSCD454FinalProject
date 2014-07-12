541ba51313a338334614e0a086194fe0a2a5331c
Nathaniel Graham <nrgraham23@gmail.com>
2014-07-12 15:48:30 -0700

    Added Attributes
    Added some attributes to character types

--- src/character/Character.java
+++ src/character/Character.java
@@ -6,5 +6,39 @@
 package character;
 
 public abstract class Character {
-
+	protected int health;
+	protected int attackPwr;
+	protected int armorVal;
+	
+	
+	public int getHealth() {
+		return this.health;
+	}
+	public int getAttackPwr() {
+		return this.attackPwr;
+	}
+	public int getArmorVal() {
+		return this.armorVal;
+	}
+	public void setHealth(int health) {
+		if(health >= 0) {
+			this.health = health;
+		} else {
+			this.health = 0;
+		}
+	}
+	public void setAttackPwr(int attackPwr) {
+		if(attackPwr >= 0) {
+			this.attackPwr = attackPwr;
+		} else {
+			this.attackPwr = 0;
+		}
+	}
+	public void setArmorVal(int armorVal) {
+		if(armorVal >= 0) {
+			this.armorVal = armorVal;
+		} else {
+			this.armorVal = 0;
+		}
+	}
 }
--- src/character/Good.java
+++ src/character/Good.java
@@ -6,5 +6,16 @@
 package character;
 
 public abstract class Good extends Character {
-
+	protected int mana;
+	
+	public int getMana() {
+		return this.mana;
+	}
+	public void setMana(int mana) {
+		if(mana >= 0) {
+			this.mana = mana;
+		} else {
+			this.mana = 0;
+		}
+	}
 }
